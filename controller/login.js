e72fb0d63cdcd899c93ce09a27e7c00f3eab1bdd
Afif <muhammad.afif81@gmail.com>
2019-05-23 14:08:12 +0700

    add register api

--- controller/login.js
+++ controller/login.js
@@ -2,35 +2,37 @@ exports.processLogin = (req,res) => {
     var email = req.body.email
     var password = req.body.password
 
-    if(req.session.username) {
+    if(req.session.email) {
         res.json({
             status: 200,
             message: "You was logged in"
         })
     }
-
-    db.Owner.findOne({
-        email: email
-    }).then(result => {
-        if(result) {
-            if(password == result.password) {
-                res.json({
-                    status: 200,
-                    message: "Logging in success"
-                })
+    else {
+        db.Owner.findOne({
+            email: email
+        }).then(result => {
+            if(result) {
+                if(password == result.password) {
+                    req.session.email = email
+                    res.json({
+                        status: 200,
+                        message: "Logging in success"
+                    })
+                }
+                else {
+                    res.json({
+                        status: 401,
+                        message: "Email or Password are wrong"
+                    })
+                }
             }
             else {
                 res.json({
                     status: 401,
-                    message: "Username or Password are wrong"
+                    message: "Email or Password are wrong"
                 })
             }
-        }
-        else {
-            res.json({
-                status: 401,
-                message: "Username or Password are wrong"
-            })
-        }
-    })
+        })
+    }
 }
