ba5d94ccb47d4d60c16c1acc8520180bc12bb8c8
Afif <muhammad.afif81@gmail.com>
2019-05-23 13:41:50 +0700

    add register api

--- controller/register.js
+++ controller/register.js
@@ -1,20 +1,17 @@
 const db = require('../models/db')
 
 exports.processRegister = (req,res) => {
+    console.log(req.body)
     var email = req.body.email
     var username = req.body.username
     var password = req.body.password
 
-    db.Owner.sync({force: false}).then(function () {
-        var ins = db.Owner.create({
-            email: email,
-            username: username,
-            password: password
-        })
-        return res.send(JSON.stringify(ins))
-    });
-
-    db.Owner.findAll().then(x => {
-        console.log(x)
-    })
+    // db.Owner.sync({force: false}).then(function () {
+    //     var ins = db.Owner.create({
+    //         email: email,
+    //         username: username,
+    //         password: password
+    //     })
+    //     return res.send(JSON.stringify(ins))
+    // });
 }
