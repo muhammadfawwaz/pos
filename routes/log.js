a6807c37ee9d69140b68fa2007e9b486b5fdad43
Afif <muhammad.afif81@gmail.com>
2019-05-25 01:59:32 +0700

    add log api

--- routes/log.js
+++ routes/log.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var log = require('../controller/log')
 
-router.get('/', log.selectLog)
+router.post('/', log.selectLog)
 
 module.exports = router;
