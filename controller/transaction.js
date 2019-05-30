b8613a7db584eca78e9dc459208907231fd98780
Afif <muhammad.afif81@gmail.com>
2019-05-30 22:22:56 +0700

    fixing cashier

--- controller/transaction.js
+++ controller/transaction.js
@@ -20,12 +20,36 @@ exports.add = (req,res) => {
         var month = momentz.tz(d.getMonth()+1,'MM','UTC').clone().tz('Asia/Jakarta').format('M')
         result.total += 1
         console.log(result.month,month)
-        if(result.month == month) result.perMonth += 1
+        if(result.month == month) {
+            result.updateAttributes({
+                perMonth: result.perMonth + 1
+            })
+        }
         else {
-            result.month = month
-            result.perMonth = 0 
+            result.updateAttributes({
+                perMonth: 0,
+                month: month
+            })
         }
     })
+
+    // db.Cashier.update({
+        
+    // },{
+    //     where: {
+    //         email: cashierEmail
+    //     }
+    // }).then(result => {
+    //     var d = new Date()
+    //     var month = momentz.tz(d.getMonth()+1,'MM','UTC').clone().tz('Asia/Jakarta').format('M')
+    //     result.total += 1
+    //     console.log(result.month,month)
+    //     if(result.month == month) result.perMonth += 1
+    //     else {
+    //         result.month = month
+    //         result.perMonth = 0 
+    //     }
+    // })
 
     db.Trans.create({
         ownerEmail: ownerEmail,
